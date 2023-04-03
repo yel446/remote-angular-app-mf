6a9c5ba8c57c869cbb461862ba1105b5c026b65c
YAMB LANDRY <landryyamb446@gmail.com>
2023-04-03 15:42:14 +0000

    update concat  😎

--- concat.js
+++ concat.js
@@ -1,11 +1,7 @@
 const concat = require("concat");
 
 (async function build() {
-  const files = [
-    "./dist/remote-angular-app-wp/runtime.js",
-    "./dist/remote-angular-app-wp/polyfills.js",
-    "./dist/remote-angular-app-wp/main.js",
-  ];
+  const files = ["./dist/runtime.js", "./dist/polyfills.js", "./dist/main.js"];
 
-  await concat(files, "./dist/remote-angular-app-wp/micro-fe.js");
+  await concat(files, "./dist/micro-fe.js");
 })();
